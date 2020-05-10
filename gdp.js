f2ec9ad270d25b13ed35e79f8aa3c3af2aa43532
Keylor Villalobos M <keylor.villalobosm@gmail.com>
2020-05-10 17:12:28 -0600

    restore

--- gdp.js
+++ gdp.js
@@ -1,198 +1,12 @@
-let categories = [{
-    name: 'Galletas',
-    id: 0
-  },
-  {
-    name: 'Muffins',
-    description: 'prueba',
-    id: 1
-  },
-  {
-    name: 'Mantequillas',
-    description: 'prueba',
-    id: 2
-  },
-  {
-    name: 'Choco Cocadas',
-    description: 'prueba',
-    id: 3
-  },
-  {
-    name: 'Nutcracker Bread',
-    description: 'prueba',
-    id: 4
-  },
-  {
-    name: 'Proballs',
-    description: 'prueba',
-    id: 5
-  },
-  {
-    name: 'Crunch Bars',
-    description: 'prueba',
-    id: 6
-  },
-  {
-    name: 'Donas',
-    description: 'prueba',
-    id: 7
-  },
-  {
-    name: 'Queques',
-    description: 'prueba',
-    id: 8
-  },
-]
-let products = [{
-    name: "Galletas de Almendra",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 5,
-    image: "gfpimg/galletas-almendra.jpg",
-    idCategory: 0,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Galletas de Cacao",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 6,
-    image: "gfpimg/galletas-cacao.jpg",
-    idCategory: 0,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "ZanaBana Muffin",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 1,
-    image: "gfpimg/zanabana-muffin.jpg",
-    idCategory: 1,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Choco ZanaBana Muffin",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 2,
-    image: "gfpimg/choco-muffin.jpg",
-    idCategory: 1,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Mantequilla de Almendras",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 3,
-    image: "gfpimg/mantequillam.jpg",
-    idCategory: 2,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Mantequilla de Maní",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 4,
-    image: "gfpimg/mantequillaa.jpg",
-    idCategory: 2,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Choco Cocadas",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 7,
-    image: "gfpimg/chocococadas.jpg",
-    idCategory: 3,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Nutcracker Bread",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 8,
-    image: "gfpimg/nutcrackerbread.jpg",
-    idCategory: 4,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Keto Nutcracker Bread",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 9,
-    image: "gfpimg/ketonutcracker.jpg",
-    idCategory: 4,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Proballs",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 10,
-    image: "gfpimg/proballs.jpg",
-    idCategory: 5,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Crunch Bars",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 11,
-    image: "gfpimg/Crunchbars.jpg",
-    idCategory: 6,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Donas",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 12,
-    image: "gfpimg/donas.jpg",
-    idCategory: 7,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Queque de Camote y Cacao",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 13,
-    image: "gfpimg/quequecamote.jpg",
-    idCategory: 8,
-    price: 2000,
-    quantity: 1
-  },
-  {
-    name: "Queque de Limón",
-    description: "6 unidades de galletas Gluten free.",
-    idProduct: 14,
-    image: "gfpimg/quequecamote.jpg",
-    idCategory: 8,
-    price: 2000,
-    quantity: 1
-  },
-]
-
-
 function getData() {
-  const response = new XMLHttpRequest()
-  response.open('GET', 'gdp.json', true)
-  response.send();
-  response.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      //categories = JSON.parse(this.responseText)
-    }
-  }
-  const resProducts = new XMLHttpRequest()
-  resProducts.open('GET', 'products.json', true)
-  resProducts.send();
-  resProducts.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      //products = JSON.parse(this.responseText)
-    }
-  }
   let section = document.querySelector('#products');
+  let dropdown = document.querySelector('#categories-menu');
   for (let category of categories) {
     section.innerHTML += `
-    <h1 class="display-4 text-center">-${category.name}-</h1>
+    <h1 id=${category.htmlId} class="display-4 text-center">-${category.name}-</h1>
+    `;
+    dropdown.innerHTML += `
+    <a class="dropdown-item" href="#${category.htmlId}">${category.name}</a>
     `;
     let cards = document.createElement('div');
     cards.classList.add('row', 'd-flex', 'justify-content-center');
@@ -227,20 +41,21 @@ function getData() {
     }
   }
 }
-getData()
-let temp2 = ''
-var numberCart = 0
-var total = 0
+
+getData();;
+let temp2 = '';
+var numberCart = 0;
+var total = 0;
 
 function cartInit() {
-  total = 0
-  numberCart = 0
-  let cart = document.getElementById('product-card')
-  let temp = products
-  let data = JSON.parse(localStorage.getItem('cart'))
+  total = 0;
+  numberCart = 0;
+  let cart = document.getElementById('product-card');
+  let temp = products;
+  let data = JSON.parse(localStorage.getItem('cart'));
   if (data) {
     data.forEach(item => {
-       numberCart += item.quantity
+      numberCart += item.quantity;
       temp.forEach(product => {
         if (item.idProduct == product.idProduct) {
           document.getElementById(item.idProduct).innerHTML = `
@@ -251,16 +66,17 @@ function cartInit() {
             <span class='moreProduct' onclick="moreProduct(${item.idProduct})"><i class="fas fa-plus"></i></span>
             </div>
           `
-        }
+        };
       });
     });
   }
+
   document.getElementById("numberCart").innerHTML = numberCart;
-  cart.innerHTML = ''
+  cart.innerHTML = '';
   if (data) {
     data.forEach(dato => {
-      let suma = parseFloat(dato.price) * dato.quantity
-      total += parseFloat(suma)
+      let suma = parseFloat(dato.price) * dato.quantity;
+      total += parseFloat(suma);
     });
     for (const product of data) {
       cart.innerHTML += `
@@ -278,25 +94,23 @@ function cartInit() {
         `
     }
     document.getElementById('total').innerHTML = `
-        <h5>Total:&#8353;${total}</h5>
-      `
-    let name = ''
-    let quantity = ''
+      <h5>Total:&#8353;${total}</h5>
+    `;
+    let name = '';
+    let quantity = '';
     data.forEach(product => {
-      name += product.name + ', precio unitario: &#8353;' + product.price + ', cantidad: ' + product.quantity + '%0D%0A'
+      name += product.name + ', precio unitario: &#8353;' + product.price + ', cantidad: ' + product.quantity + '%0D%0A';
     });
     document.getElementById('whatsapp').innerHTML = `
       <a class="btn btn-primary" href="https://web.whatsapp.com/send?phone=50685860314&text=Lista de productos:%0D%0A${name} %0D%0ATotal:&#8353;${total}" target="_blank">Share via Whatsapp</a>
-      `
+      `;
   }
 }
-function payPal(){
-  let data = JSON.parse(localStorage.getItem('cart'))
-  console.log(data)
-  document.getElementById('')
+
+function payPal() {
   paypal.Buttons({
     createOrder: function (data, actions) {
-      
+
       // This function sets up the details of the transaction, including the amount and line item details.
       return actions.order.create({
         purchase_units: [{
@@ -310,110 +124,81 @@ function payPal(){
       // This function captures the funds from the transaction.
       return actions.order.capture().then(function (details) {
         // This function shows a transaction success message to your buyer.
-        localStorage.clear()
-        total = 0
+        localStorage.clear();
+        total = 0;
         document.getElementById('total').innerHTML = `
           <h5>Total:&#8353;${total}</h5>
-          `
-        document.getElementById("numberCart").innerHTML = 0
+          `;
+        document.getElementById("numberCart").innerHTML = 0;
         alert('Transaction completed by ' + details.payer.name.given_name);
         products.forEach((product, index) => {
           document.getElementById(product.idProduct).innerHTML = `
-                <a onclick="addCart(${product.idProduct})" class="btn btn-success add-cart cart1">Añadir al Carrito</a>
-              `
+            <a onclick="addCart(${product.idProduct})" class="btn btn-success add-cart cart1">Añadir al Carrito</a>
+          `;
         });
-        cartInit()
+        cartInit();
       });
     }
   }).render('#paypal-button-container');
 }
-payPal()
+
+payPal();
+
 function moreProduct(id) {
   let data = JSON.parse(localStorage.getItem('cart'))
   data.forEach(product => {
     if (product.idProduct == id) {
-      product.quantity = product.quantity + 1
+      product.quantity = product.quantity + 1;
     }
   });
-  localStorage.setItem('cart', JSON.stringify(data))
-  cartInit()
+  localStorage.setItem('cart', JSON.stringify(data));
+  cartInit();
 }
 
 function lessItems(id) {
-  let data = JSON.parse(localStorage.getItem('cart'))
+  let data = JSON.parse(localStorage.getItem('cart'));
   data.forEach(product => {
     if (product.idProduct == id && product.quantity > 1) {
-      product.quantity = product.quantity - 1
+      product.quantity = product.quantity - 1;
     }
   });
-  localStorage.setItem('cart', JSON.stringify(data))
-  cartInit()
+  localStorage.setItem('cart', JSON.stringify(data));
+  cartInit();
 }
 
 function deleteProduct(id) {
   let cart = JSON.parse(localStorage.getItem('cart'))
   cart.forEach((product, index) => {
     if (id == product.idProduct) {
-      cart.splice(index, 1)
+      cart.splice(index, 1);
       document.getElementById(product.idProduct).innerHTML = `
         <a onclick="addCart(${product.idProduct})" class="btn btn-success add-cart cart1">Añadir al Carrito</a>
-      `
+      `;
     }
   });
-  localStorage.setItem('cart', JSON.stringify(cart))
-  cartInit()
+  localStorage.setItem('cart', JSON.stringify(cart));
+  cartInit();
 }
-cartInit()
+cartInit();
 
 function addCart(id) {
   let data;
-  let cart = []
+  let cart = [];
   if (localStorage.getItem('cart')) {
-    data = JSON.parse(localStorage.getItem('cart'))
-    cart = data
+    data = JSON.parse(localStorage.getItem('cart'));
+    cart = data;
   }
   products.forEach(item => {
     if (item.idProduct == id) {
-      cart.push(item)
+      cart.push(item);
     }
   });
-  localStorage.setItem('cart', JSON.stringify(cart))
-  numberCart = numberCart + 1
-  let iconCart = document.getElementById("numberCart")
-  iconCart.innerHTML = numberCart
+  localStorage.setItem('cart', JSON.stringify(cart));
+  numberCart = numberCart + 1;
+  let iconCart = document.getElementById("numberCart");
+  iconCart.innerHTML = numberCart;
   document.getElementById(id).innerHTML = `
   <a style='color:white;' class="btn btn-success add-cart cart1">Añadido al carrito</a>
-  `
-  cartInit()
-} {
-  /* <style>
-  #section1 {
-    height: 600px;
-    background-color: pink;
-  }
-
-  #section2 {
-    height: 600px;
-    background-color: yellow;
-  }
-  </style>
-  </head>
-  <body>
-
-  <h1>Smooth Scroll</h1>
-
-  <div class="main" id="section1">
-    <h2>Section 1</h2>
-    <p>Click on the link to see the "smooth" scrolling effect.</p>
-    <a href="#section2">Click Me to Smooth Scroll to Section 2 Below</a>
-    <p>Note: Remove the scroll-behavior property to remove smooth scrolling.</p>
-  </div>
-
-  <div class="main" id="section2">
-    <h2>Section 2</h2>
-    <a href="#section1">Click Me to Smooth Scroll to Section 1 Above</a>
-  </div>
-
-  </body>
-  </html> */
+  `;
+  cartInit();
 }

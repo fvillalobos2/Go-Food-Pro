be8ac67daae4e914901e11c796277bfba290bac9
JessAdolfoR <50562002+JessAdolfoR@users.noreply.github.com>
2020-05-10 00:13:57 -0400

    more and less products

--- gdp.js
+++ gdp.js
@@ -271,6 +271,11 @@ function cartInit() {
         if (item.idProduct == product.idProduct) {
           document.getElementById(item.idProduct).innerHTML = `
             <a style='color:white;' class="btn btn-success add-cart cart1">Añadido al carrito</a>
+            <div class="d-flex">
+            <span class='lessProduct' onclick = "lessItems(${item.idProduct})">-</span>
+              <p><b>${item.quantity}</b></p>
+            <span class='moreProduct' onclick="moreProduct(${item.idProduct})"><i class="fas fa-plus"></i></span>
+            </div>
           `
         }
       });
@@ -286,12 +291,16 @@ function cartInit() {
     for (const product of data) {
       cart.innerHTML += `
         <div class='d-flex'>
-        <span class='delete-product' onclick="deleteProduct(${product.idProduct})">x</span>
+        <span class='delete-product' onclick="deleteProduct(${product.idProduct})"><i class="fas fa-trash-alt"></i></span>
           <h5>${product.name}</h5>
           <p>${product.description}</p>
           <p class='mr-3'><b>&#8353;${product.price}</b></p>
-          <p><b>${product.quantity}</b></p>
-        </div>
+            <div class="d-flex">
+            <span class='lessProduct' onclick = "lessItems(${product.idProduct})">-</span>
+              <p><b>${product.quantity}</b></p>
+            <span class='moreProduct' onclick="moreProduct(${product.idProduct})"><i class="fas fa-plus"></i></span>
+            </div>
+          </div>
         `
     }
     document.getElementById('total').innerHTML = `
@@ -300,15 +309,36 @@ function cartInit() {
     let name = ''
     let quantity = ''
     data.forEach(product => {
-      name += product.name + ', precio: ' + product.price + ', cantidad: ' + product.quantity + '%0D%0A'
+      name += product.name + ', precio unitario: &#8353;' + product.price + ', cantidad: ' + product.quantity + '%0D%0A'
       console.log(name)
     });
     document.getElementById('whatsapp').innerHTML = `
-      <a class="btn btn-primary" href="https://web.whatsapp.com/send?text=Lista de productos:%0D%0A${name} %0D%0ATotal:&#8353;${total}" target="_blank">Share via Whatsapp</a>
+      <a class="btn btn-primary" href="https://web.whatsapp.com/send?phone=50685860314&text=Lista de productos:%0D%0A${name} %0D%0ATotal:&#8353;${total}" target="_blank">Share via Whatsapp</a>
       `
   }
 }
-
+  function moreProduct(id){
+    let data = JSON.parse(localStorage.getItem('cart'))
+    data.forEach(product => {
+      if(product.idProduct == id){
+        product.quantity = product.quantity + 1 
+      }
+    });
+    console.log(data)
+    localStorage.setItem('cart', JSON.stringify(data))
+    cartInit()
+  }
+  function lessItems(id){
+    let data = JSON.parse(localStorage.getItem('cart'))
+    data.forEach(product => {
+      if(product.idProduct == id && product.quantity > 1){
+        product.quantity = product.quantity - 1 
+      }
+    });
+    console.log(data)
+    localStorage.setItem('cart', JSON.stringify(data))
+    cartInit()
+  }
 function deleteProduct(id) {
   let cart = JSON.parse(localStorage.getItem('cart'))
   cart.forEach((product, index) => {

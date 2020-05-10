8ed63d60f7878d0b2f98c342959eff9eccfb6942
Keylor Villalobos M <keylor.villalobosm@gmail.com>
2020-05-10 14:18:24 -0600

    code cleanup, dropdown, json remove, unnecesary jquery commented

---
+++ list.js
@@ -0,0 +1,182 @@
+let products = [{
+        name: "Galletas de Almendra",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 5,
+        image: "gfpimg/galletas-almendra.jpg",
+        idCategory: 0,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Galletas de Cacao",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 6,
+        image: "gfpimg/galletas-cacao.jpg",
+        idCategory: 0,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "ZanaBana Muffin",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 1,
+        image: "gfpimg/zanabana-muffin.jpg",
+        idCategory: 1,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Choco ZanaBana Muffin",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 2,
+        image: "gfpimg/choco-muffin.jpg",
+        idCategory: 1,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Mantequilla de Almendras",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 3,
+        image: "gfpimg/mantequillam.jpg",
+        idCategory: 2,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Mantequilla de Maní",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 4,
+        image: "gfpimg/mantequillaa.jpg",
+        idCategory: 2,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Choco Cocadas",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 7,
+        image: "gfpimg/chocococadas.jpg",
+        idCategory: 3,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Nutcracker Bread",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 8,
+        image: "gfpimg/nutcrackerbread.jpg",
+        idCategory: 4,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Keto Nutcracker Bread",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 9,
+        image: "gfpimg/ketonutcracker.jpg",
+        idCategory: 4,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Proballs",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 10,
+        image: "gfpimg/proballs.jpg",
+        idCategory: 5,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Crunch Bars",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 11,
+        image: "gfpimg/Crunchbars.jpg",
+        idCategory: 6,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Donas",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 12,
+        image: "gfpimg/donas.jpg",
+        idCategory: 7,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Queque de Camote y Cacao",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 13,
+        image: "gfpimg/quequecamote.jpg",
+        idCategory: 8,
+        price: 2000,
+        quantity: 1
+    },
+    {
+        name: "Queque de Limón",
+        description: "6 unidades de galletas Gluten free.",
+        idProduct: 14,
+        image: "gfpimg/quequecamote.jpg",
+        idCategory: 8,
+        price: 2000,
+        quantity: 1
+    },
+];
+let categories = [{
+        name: 'Galletas',
+        htmlId: 'galletas',
+        description: 'prueba',
+        id: 0
+    },
+    {
+        name: 'Muffins',
+        htmlId: 'muffins',
+        description: 'prueba',
+        id: 1
+    },
+    {
+        name: 'Mantequillas',
+        htmlId: 'mantequillas',
+        description: 'prueba',
+        id: 2
+    },
+    {
+        name: 'Choco Cocadas',
+        htmlId: 'choco-cocadas',
+        description: 'prueba',
+        id: 3
+    },
+    {
+        name: 'Nutcracker Bread',
+        htmlId: 'nutcracker-bread',
+        description: 'prueba',
+        id: 4
+    },
+    {
+        name: 'Proballs',
+        htmlId: 'proballs',
+        description: 'prueba',
+        id: 5
+    },
+    {
+        name: 'Crunch Bars',
+        htmlId: 'crunch',
+        description: 'prueba',
+        id: 6
+    },
+    {
+        name: 'Donas',
+        htmlId: 'donas',
+        description: 'prueba',
+        id: 7
+    },
+    {
+        name: 'Queques',
+        htmlId: 'queques',
+        description: 'prueba',
+        id: 8
+    },
+];

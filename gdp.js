79eebf12d51887751cd353a9a2041d06b8581361
Keylor Villalobos M <keylor.villalobosm@gmail.com>
2020-05-10 13:47:03 -0600

    Update gdp.js

--- gdp.js
+++ gdp.js
@@ -1,31 +1,3 @@
-{
-  /* <script>
-   $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-      console.log('hola')
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-
-        // Store hash
-        var hash = this.galletas;
-
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    })
-  }); 
-  </script> */
-}
 let products = [{
     name: "Galletas de Almendra",
     description: "6 unidades de galletas Gluten free.",

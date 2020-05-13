a572f8ea68d8baef4d9e185fd6910b60f0d49626
fvillalobos2 <48699528+fvillalobos2@users.noreply.github.com>
2020-05-12 23:34:55 -0600

    May-12 Css Changes

--- list.js
+++ list.js
@@ -1,109 +1,127 @@
 let products = [{
         name: "Galletas de Almendra",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 5,
+        description: "<p>Galletas a base de nuestra cremosa mantequilla de almendras con crujientes nibs de cacao.</p> <p> Paquete de <b> 6 unidades</b></p><p><b>Intercambio nutricional:</b> 1 galleta =<br> 2 grasas, 1 proteína y 1 carbohidrato</p><p><b>Ingredientes:</b>  Almendras, cacao nibs recubiertos de chocolate al 70%, claras de huevo, azúcar de coco, almidón de yuca, bicarbonato de sodio. Contiene: Almendras y claras de huevo. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 1,
         image: "gfpimg/galletas-almendra.jpg",
         idCategory: 0,
-        price: 2000,
+        price: 5500,
         quantity: 1
     },
     {
         name: "Galletas de Cacao",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 6,
+        description: "<p>Galletas de cacao intenso sin harinas.</p> <p> Paquete de <b> 6 unidades</b></p><p><b>Intercambio nutricional:</b> 1 galleta =<br> 1 ½ grasas, ½ proteína y ½ carbohidrato</p><p><b>Ingredientes:</b>  Cacao nibs recubiertos de chocolate al 70%, mantequilla de almendras, mantequilla de maní, azúcar de coco, claras de huevo, cocoa en polvo, bicarbonato de sodio. Contiene: Almendras, maní y huevo. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 2,
         image: "gfpimg/galletas-cacao.jpg",
         idCategory: 0,
-        price: 2000,
+        price: 5000,
         quantity: 1
     },
     {
         name: "ZanaBana Muffin",
-        description: "Presentación: Paquete de 6 unidades Muffins a base de zanahoria con banano sin azúcar",
-        idProduct: 1,
+        description: "<p>Muffins a base de zanahoria con banano sin azúcar.</p> <p> Paquete de <b> 6 unidades</b></p><p><b>Intercambio nutricional:</b> 1 muffin = <br> 1 carbohidrato y ½ grasa</p><p><b>Ingredientes:</b> Bananos, avena, claras de huevo, zanahorias, aceite de coco, sucralosa, sal, especias (menos del 2%) y bicarbonato de sodio. Contiene huevo. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 3,
         image: "gfpimg/zanabana-muffin.jpg",
         idCategory: 1,
-        price: 2000,
+        price: 5500,
         quantity: 1
     },
     {
         name: "Choco ZanaBana Muffin",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 2,
+        description: "<p>Muffins a base de zanahoria con banano sin azúcar con chips de chocolate oscuro</p> <p> Paquete de <b> 6 unidades</b></p><p><b>Intercambio nutricional:</b> 1 muffin =<br> 1 carbohidrato, 1 grasa y ½ proteína</p><p><b>Ingredientes:</b> Bananos, avena, claras de huevo, zanahorias, chocolate semiamargo, aceite de  coco, sucralosa, sal, especias (menos del 2%) y bicarbonato de sodio. Contiene: huevo. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 4,
         image: "gfpimg/choco-muffin.jpg",
         idCategory: 1,
-        price: 2000,
+        price: 5500,
         quantity: 1
     },
     {
-        name: "Mantequilla de Almendras",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 3,
+        name: "Mantequilla de Maní Sabor a Leche Condensada Sin Lácteos y Sin Azúcar",
+        description: "<p>Frasco de vidrio de <b>265g</b></p><p><b>Intercambio nutricional:</b> 1 cucharada = 1,5 grasas</p><p><b>Ingredientes:</b> Maní, extracto de stevia como endulzante, sabor a leche condensada. Contiene maní. Puede contener trazas de semillas o nueces.</p>",
+        idProduct: 7,
         image: "gfpimg/mantequillam.jpg",
         idCategory: 2,
-        price: 2000,
+        price: 5500,
         quantity: 1
     },
     {
-        name: "Mantequilla de Maní",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 4,
+        name: "Mantequilla de Maní 100% Natural",
+        description: "<p>Frasco de vidrio de <b>265g</b></p><p><b>Intercambio nutricional:</b> 1 cucharada = 1,5 grasas</p><p><b>Ingredientes:</b> Maní. Puede contener trazas de semillas o nueces.</p>",
+        idProduct: 6,
         image: "gfpimg/mantequillaa.jpg",
         idCategory: 2,
-        price: 2000,
+        price: 5000,
         quantity: 1
     },
     {
-        name: "Choco Cocadas",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 7,
+        name: "Mantequilla de Almendras 100% Natural",
+        description: "<p>Frasco de vidrio de <b>265g</b></p><p><b>Intercambio nutricional:</b> 1 cucharada = 1,5 grasas</p><p><b>Ingredientes:</b>Almendras. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 5,
+        image: "gfpimg/mantequillaa.jpg",
+        idCategory: 2,
+        price: 6500,
+        quantity: 1
+    },
+    {
+        name: "Mantequilla de Almendras Sabor a Leche Condensada Sin Lácteos y Sin Azúcar",
+        description: "<p>Frasco de vidrio de <b>265g</b></p><p><b>Intercambio nutricional:</b> 1 cucharada = 1,5 grasas</p><p><b>Ingredientes:</b> Almendras, extracto de stevia como endulzante, sabor a leche condensada. Contiene almendras. Puede contener trazas de semillas o nueces.</p>",
+        idProduct: 8,
+        image: "gfpimg/mantequillaa.jpg",
+        idCategory: 2,
+        price: 7500,
+        quantity: 1
+    },
+    {
+        name: "Keto Cocadas",
+        description: "<p>Galletas a base de coco y cacao puro.</p> <p> Paquete de <b> 200 gramos</b></p><p><b>Intercambio nutricional:</b> 3 cocadas =<br> 1 galleta: 2 ½ grasas</p><p><b>Ingredientes:</b> Coco, claras de huevo, eritritol, mantequilla de almendras, aceite de coco, cocoa pura, y bicarbonato de sodio. Contiene: almendras y huevo. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 9,
         image: "gfpimg/chocococadas.jpg",
         idCategory: 3,
-        price: 2000,
+        price: 5500,
         quantity: 1
     },
     {
         name: "Nutcracker Bread",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 8,
+        description: "<p>Pan a base de semillas mixtas con fibra natural.</p> <p> Unidad de <b> 750 gramos</b><br>(de 10 a 15 tajadas)</p><p><b>Intercambio nutricional:</b> 1 rebanada de 50g = 2 grasas</p><p><b>Ingredientes:</b> Agua, semillas mixtas (girasol, linaza, chía, almendras, ajonjolí, calabaza) avena, ƒibra, aceite de coco, agave azul orgánico, sal. Contiene: almendras y ajonjolí. Puede contener trazas de gluten.</p>",
+        idProduct: 10,
         image: "gfpimg/nutcrackerbread.jpg",
         idCategory: 4,
-        price: 2000,
+        price: 6000,
         quantity: 1
     },
     {
         name: "Keto Nutcracker Bread",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 9,
+        description: "<p>Pan a base de semillas mixtas con fibra natural sin avena y sin azúcares añadidos.</p> <p> Unidad de <b> 750 gramos</b><br>(de 10 a 15 tajadas)</p><p><b>Intercambio nutricional:</b> 1 rebanada de 50g = Carbohidratos totales: 3g Carbohidratos netos : 0g  Grasas: 2</p><p><b>Ingredientes:</b> Agua, semillas mixtas (girasol, linaza, chía, almendras, ajonjolí, calabaza), ƒibra, aceite de coco, sal. Contiene: almendras y ajonjolí. Puede contener trazas de gluten.</p>",
+        idProduct: 11,
         image: "gfpimg/ketonutcracker.jpg",
         idCategory: 4,
-        price: 2000,
+        price: 6000,
         quantity: 1
     },
-    {
-        name: "Proballs",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 10,
-        image: "gfpimg/proballs.jpg",
-        idCategory: 5,
-        price: 2000,
-        quantity: 1
-    },
+    // {
+    //     name: "Proballs",
+    //     description: "6 unidades de galletas Gluten free.",
+    //     idProduct: 10,
+    //     image: "gfpimg/proballs.jpg",
+    //     idCategory: 5,
+    //     price: 2000,
+    //     quantity: 1
+    // },
     {
         name: "Crunch Bars",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 11,
+        description: "<p>Barritas a base de arroz tostado y mantequilla de almendras 100% natural.</p> <p> Paquete de <b> 6 unidades</b><br></p><p><b>Intercambio nutricional:</b> 1 barrita =<br> 1 grasa, ½ proteína y 1 carbohidrato</p><p><b>Ingredientes:</b> Arroz tostado, agave azul orgánico, mantequilla de almendras (sin azúcar o aceites añadidos), chocolate semiamargo, proteína aislada de suero de leche. Contiene: almendras y suero de leche. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 12,
         image: "gfpimg/Crunchbars.jpg",
         idCategory: 6,
-        price: 2000,
+        price: 4500,
         quantity: 1
     },
     {
         name: "Donas",
-        description: "6 unidades de galletas Gluten free.",
-        idProduct: 12,
+        description: "<p>Donitas a base de camote como primer ingrediente con chocolate semiamargo y cremosa mantequilla de maní.</p> <p> Paquete de <b> 6 unidades</b></p><p><b>Intercambio nutricional:</b> 1 dona = <br> ½ carbohidrato y 1 grasa</p><p><b>Ingredientes:</b> Camote, chocolate semiamargo, leche de coco, avena, mantequilla de maní (sin azúcares o aceites añadidos). Contiene: maní. Puede contener trazas de gluten, semillas o nueces.</p>",
+        idProduct: 13,
         image: "gfpimg/donas.jpg",
         idCategory: 7,
-        price: 2000,
+        price: 3900,
         quantity: 1
     },
     // {
@@ -144,8 +162,8 @@ let categories = [{
         id: 2
     },
     {
-        name: 'Choco Cocadas',
-        htmlId: 'choco-cocadas',
+        name: 'Cocadas',
+        htmlId: 'cocadas',
         description: 'prueba',
         id: 3
     },
@@ -155,12 +173,12 @@ let categories = [{
         description: 'prueba',
         id: 4
     },
-    {
-        name: 'Proballs',
-        htmlId: 'proballs',
-        description: 'prueba',
-        id: 5
-    },
+    // {
+    //     name: 'Proballs',
+    //     htmlId: 'proballs',
+    //     description: 'prueba',
+    //     id: 5
+    // },
     {
         name: 'Crunch Bars',
         htmlId: 'crunch',
